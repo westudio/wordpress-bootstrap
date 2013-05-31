67ead67a3ff7fe13ead2d67564ebc7ddd10d097c
Lionel Gaillard <lionel@wxrstudios.com>
2013-05-31 16:18:36 +0200

    Added links

--- wp-content/themes/bootstrap/block-links.php
+++ wp-content/themes/bootstrap/block-links.php
@@ -1,59 +1,66 @@
 <?php
-
 if ($categories = get_terms('link_category')):
-    var_dump($categories);
 ?>
 
 <section class="links">
 
+    <h2 class="links-heading"><?php _e('Links', 'bootstrap'); ?></h2>
+
 <?php
     foreach ($categories as $category):
         if ($links = get_bookmarks(array('category' => $category->term_id))):
-            var_dump($links)
 ?>
-
 
     <div class="links-category">
 
-        <h2><?php echo $category->name; ?></h2>
+        <h3 class="links-category-heading"><?php echo $category->name; ?></h3>
 
-        <ul class="media-list">
+        <ul class="links-list">
 <?php
             foreach ($links as $link):
 ?>
-                <li class="media">
-<?php
-                if ($link->link_image):
-?>
-                    <a class="pull-left" href="<?php echo $link->link_url; ?>" target="<?php echo $link->link_target; ?>">
-                        <img src="<?php echo $link->link_image; ?>" alt="" class="media-object">
+                <li class="link">
+                    <a href="<?php echo $link->link_url; ?>"
+                       target="<?php echo $link->link_target; ?>"
+                       title="<?php echo $link->link_description; ?>">
+                        <?php echo $link->link_name; ?>
                     </a>
-<?php
-                endif;
-?>
-                    <div class="media-body">
-                        <h4 class="media-heading">
-                            <a href="<?php echo $link->link_url; ?>" target="<?php echo $link->link_target; ?>"><?php echo $link->link_name; ?></a>
-                        </h4>
-<?php
-                if ($link->link_description):
-?>
-                        <p><?php echo $link->link_description; ?></p>
-<?php
-                endif;
-?>
-                    </div>
-                    
                 </li>
 <?php
             endforeach;
 ?>
         </ul>
-    </div>
 
 <?php
-    endforeach
+        endif;
+    endforeach;
 ?>
+
+</section>
+
+<?php
+elseif ($links = get_bookmarks()):
+?>
+
+<section class="links">
+
+    <h2 class="links-heading"><?php _e('Links', 'bootstrap'); ?></h2>
+
+        <ul class="links-list">
+<?php
+    foreach ($links as $link):
+?>
+                <li class="link">
+                    <a href="<?php echo $link->link_url; ?>"
+                       target="<?php echo $link->link_target; ?>"
+                       title="<?php echo $link->link_description; ?>">
+                        <?php echo $link->link_name; ?>
+                    </a>
+                </li>
+<?php
+    endforeach;
+?>
+        </ul>
 
 </section>
 
