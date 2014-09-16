f964f27a5da702f7f5587472f128566de6dffe52
Lionel Gaillard <lionel@we-studio.ch>
2014-09-16 10:36:33 +0200

    Added front-page.php

---
+++ wp-content/themes/bootstrap/front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Front Page
+ */
+
+if ('posts' === get_option('show_on_front')):
+    include dirname(__FILE__).'/archive.php';
+else:
+    include dirname(__FILE__).'/page-home.php';
+endif;
+
+?>
