87147bfaef8fc2758d6194906f7041820f60cea0
Lionel Gaillard <lionel.gaillard@wxrstudios.com>
2015-01-28 09:55:34 +0100

    Updated Maintenance

--- wp-content/plugins/maintenance/js/init.js
+++ wp-content/plugins/maintenance/js/init.js
@@ -1,8 +1,5 @@
 jQuery(window).ready(function(){
 		jQuery.fn.tzCheckbox = function(options){
-		
-		// Default On / Off labels:
-		
 		options = jQuery.extend({
 			labels : ['ON','OFF']
 		},options);
@@ -10,38 +7,59 @@ jQuery(window).ready(function(){
 		return this.each(function(){
 			var originalCheckBox = jQuery(this),
 				labels = [];
-
-			// Checking for the data-on / data-off HTML5 data attributes:
 			if(originalCheckBox.data('on')){
 				labels[0] = originalCheckBox.data('on');
 				labels[1] = originalCheckBox.data('off');
 			}
 			else labels = options.labels;
-
-			// Creating the new checkbox markup:
 			var checkBox = jQuery('<span>');
 				 checkBox.addClass(this.checked?' tzCheckBox checked':'tzCheckBox');
 			     checkBox.prepend('<span class="tzCBContent">'+labels[this.checked?0:1]+ '</span><span class="tzCBPart"></span>');
-
-			// Inserting the new checkbox, and hiding the original:
 			checkBox.insertAfter(originalCheckBox.hide());
 
 			checkBox.click(function(){
 				checkBox.toggleClass('checked');
-				
 				var isChecked = checkBox.hasClass('checked');
-				
-				// Synchronizing the original checkbox:
 				originalCheckBox.attr('checked',isChecked);
 				checkBox.find('.tzCBContent').html(labels[isChecked?0:1]);
 			});
-			
-			// Listening for changes on the original and affecting the new one:
+
 			originalCheckBox.bind('change',function(){
 				checkBox.click();
 			});
 		});
 	};
 	
-	jQuery('#ch_location').tzCheckbox({labels:['On','Off']});
+	jQuery('#state').tzCheckbox({labels:['On','Off']});
+		var vColorPickerOptions = {
+			defaultColor: false,
+			change: function(event, ui){},
+			clear: function() {},
+			hide: true,
+			palettes: true
+		};
+	
+	jQuery('#body_bg_color, #font_color, #body_bg_blur_color').wpColorPicker(vColorPickerOptions);
+	
+	if (jQuery('.select2_customize, .multiple-select-mt').length > 0) {
+		jQuery('.select2_customize, .multiple-select-mt').select2({});
+	}
+	
+	if (jQuery('#503_enabled').length > 0) {
+		if (jQuery('#503_enabled').prop("checked")) {
+			jQuery('#gg_analytics_id').prop('disabled', true);
+		} else {
+			jQuery('#gg_analytics_id').prop('disabled', false);
+		}
+	}
+	
+	jQuery('#503_enabled').on('change', function() {
+		if (jQuery(this).prop("checked")) {
+			jQuery('#gg_analytics_id').prop('disabled', true);
+		} else {
+			jQuery('#gg_analytics_id').prop('disabled', false);
+		}
+		
 	});
+	
+});
