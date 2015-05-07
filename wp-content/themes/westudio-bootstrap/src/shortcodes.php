a1636e375bbd9e383821647f8db5700b52382b32
Lionel Gaillard <lionel.gaillard@wxrstudios.com>
2015-05-07 17:22:19 +0200

    Fixed template part in wb_shortcode_flash_news()

--- wp-content/themes/westudio-bootstrap/src/shortcodes.php
+++ wp-content/themes/westudio-bootstrap/src/shortcodes.php
@@ -2,7 +2,7 @@
 
 function wb_shortcode_flash_news ($attributes)
 {
-    get_template_part('partials/flash-news');
+    get_template_part('blocks/flash-news');
 }
 
 add_shortcode('wb_flash_news', 'wb_shortcode_flash_news');
