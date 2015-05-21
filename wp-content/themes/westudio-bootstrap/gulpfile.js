e02b5ebb71a0198816ef770e2db2219c2c888cc5
Lionel Gaillard <lionel.gaillard@wxrstudios.com>
2015-05-21 12:55:09 +0200

    Added gulp-livereload

--- wp-content/themes/westudio-bootstrap/gulpfile.js
+++ wp-content/themes/westudio-bootstrap/gulpfile.js
@@ -2,24 +2,24 @@ var concat     = require('gulp-concat');
 var gulp       = require('gulp');
 var jshint     = require('gulp-jshint');
 var less       = require('gulp-less');
+var livereload = require('gulp-livereload');
 var sourcemaps = require('gulp-sourcemaps');
 var spawn      = require('child_process').spawn;
 var uglify     = require('gulp-uglify');
 var util       = require('gulp-util');
 
-var p;
+var connected = false;
+var port      = 8000;
 
 function report (e) {
-  console.log(e.message);
+    util.log(e.message);
 }
 
-gulp.task('default', [
-  'scripts',
-  'styles'
-]);
+gulp.task('default', ['watch']);
+
+gulp.task('build', ['scripts', 'styles']);
 
 gulp.task('scripts', ['scripts:lint'], function () {
-
   gulp.src([
     'assets/vendor/bootstrap/js/transition.js',
     'assets/vendor/bootstrap/js/alert.js',
@@ -50,6 +50,7 @@ gulp.task('scripts', ['scripts:lint'], function () {
     .pipe(concat('main.min.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('assets/scripts/dist'))
+    .pipe(livereload())
   ;
 
 });
@@ -57,7 +58,7 @@ gulp.task('scripts', ['scripts:lint'], function () {
 gulp.task('scripts:lint', function () {
   return gulp
     .src('assets/scripts/src/**/*.js')
-    .pipe(jshint({ esnext: true })).on('error', report)
+    .pipe(jshint()).on('error', report)
     .pipe(jshint.reporter('default'))
   ;
 });
@@ -70,26 +71,12 @@ gulp.task('styles', function () {
     .pipe(concat('main.min.css'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('assets/styles/dist'))
+    .pipe(livereload())
   ;
 });
 
-gulp.task('watch', function () {
-  function reload () {
-    util.log('Reloading gulpfile...');
-
-    if (p) {
-      p.kill();
-    }
-
-    p = spawn('gulp', ['watching'], { stdio: 'inherit' });
-  }
-
-  reload();
-
-  return gulp.watch(__filename, reload);
-});
-
-gulp.task('watching', function (done) {
+gulp.task('watch', function (done) {
+  livereload.listen();
   gulp.watch('assets/scripts/src/**/*', ['scripts']);
   gulp.watch('assets/styles/src/**/*', ['styles']);
 });
